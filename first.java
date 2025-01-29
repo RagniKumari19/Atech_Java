5e18239237c89d50591a177ac421907c809c4aa0
RagniKumari19 <ragnikumari1907@gmail.com>
2025-01-29 20:19:37 +0530

    demo1

--- first.java
+++ first.java
@@ -1,6 +1,86 @@
 public class first {
     public static void main(String[] s){
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//        int num = 5;
+//
+////        Casting
+//        float n = num;
+//        System.out.println(n);
+//        float n = 5.9999f;
+////        int num = n;
+////        write like this
+//        int num = (int)n;
+//        System.out.println(num);
+
+//        int num =130;
+//        byte r = (byte)num;
+//        System.out.println(r);
+//        char c ='A';
+//        int d =c;
+//        System.out.println(d);
+//        Implicit type casting
+         int a = 23;
+         float b =a;
+         long c= a;
+         double d = a;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+        long e =13;
+        float f = e;
+        double g = e;
+        System.out.println(f);
+        System.out.println(g);
+
+        float h =34f;
+        double i = h;
+        System.out.println(i);
+
+//        Explicit casting
+         double a1 = 42d;
+         float b1 = (float)a1;
+         long c1 = (long)a1;
+         int d1 = (int)a1;
+         short e1 = (short)a1;
+         byte f1 = (byte)a1;
+        System.out.println("float is " +b1);
+        System.out.println("long is "+c1);
+        System.out.println("int is "+d1);
+        System.out.println("short is "+e1);
+        System.out.println("Byte is "+f1);
+
+        long a2 = 69;
+        byte b2 = (byte)a2;
+        short c2 = (short)a2;
+        int d2 = (int)a2;
+        char e2 = (char)a2;
+        System.out.println(b2);
+        System.out.println(c2);
+        System.out.println(d2);
+        System.out.println(e2);
+
+
+//        Arithmetic operators
+        int n = 23;
+        n +=9;
+        System.out.println(n);
+        n -=3;
+        System.out.println(n);
+        n *= 5;
+        System.out.println(n);
+        n /= 3;
+        System.out.println(n);
+        n %=6;
+        System.out.println(n);
+
+
+
+
+
+
+
 
     }
 }
