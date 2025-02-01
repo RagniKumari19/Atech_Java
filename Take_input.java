a0527da68bf9b6f749108a23532cb56a049f764c
RagniKumari19 <ragnikumari1907@gmail.com>
2025-02-01 14:16:45 +0530

    take input

--- Take_input.java
+++ Take_input.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class Take_input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two number");
         int a = sc.nextInt();
 int b = sc.nextInt();
-        System.out.println("print"+a +b);
+
+        System.out.println(++a);
+        System.out.println(a--);
+
+
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a^b);
 
 
 
