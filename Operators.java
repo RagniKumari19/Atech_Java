2250f4cdec2e5c20a75aad77224ed9db7e67e490
RagniKumari19 <ragnikumari1907@gmail.com>
2025-02-01 14:10:54 +0530

    operators

---
+++ Operators.java
@@ -0,0 +1,37 @@
+public class Operators {
+    public static void main(String[] args){
+//        int a = 5;
+//        int b= 6;
+//        System.out.println(a==b);
+//
+//        logical operators
+//        boolean a = true;
+//        boolean b = false;
+//        System.out.println(a ^ b);
+////        left shift
+        int a1 = 5;
+        int b1 = a1>>1;
+
+        System.out.println(b1);
+
+
+//        int a = 5;
+//        int b = ++a *2;
+//        System.out.println(b);
+
+        int x = 10;
+        x += 5*2;
+        System.out.println(x);
+
+        int a = 5, b =9;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a^b);
+
+
+
+
+
+
+    }
+}
