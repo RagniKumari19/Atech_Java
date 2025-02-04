a047892cf3b5771a5b788dd7750553ad9ac8e9a9
RagniKumari19 <ragnikumari1907@gmail.com>
2025-02-04 17:03:14 +0530

    Number_guessing game

---
+++ Number_guessing.java
@@ -0,0 +1,37 @@
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Number_guessing {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random= new Random();
+        int num = random.nextInt(100);
+        int input,score = 100;
+        int attempts=0;
+        boolean matched = false;
+        while(!matched){
+            System.out.println("Enter a number of your choice:");
+             input = sc.nextInt();
+            if(input==num){
+                System.out.println("Hurray! you guess it right");
+                System.out.println("your score is:" +score);
+                System.out.println("No. of attempts--> "+attempts);
+                matched=true; //to end the loop here
+
+            } else if (input>num) {
+                System.out.println("Think small");
+                score--;
+                attempts++;
+            }
+            else {
+                System.out.println("Think big");
+                score--;
+                attempts++;
+            }
+;        }
+
+    }
+}
